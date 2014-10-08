cad36a3486ccc63bbbb3e5ad31cdb0fda973d55f
Shubham Singh <shubhamsingh147@gmail.com>
2014-10-08 11:16:26 +0530

    local TV added

--- src/com/android/Ptextu/MainActivity.java
+++ src/com/android/Ptextu/MainActivity.java
@@ -8,16 +8,18 @@ import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements OnClickListener {
 
 	EditText et;
-
+	TextView txt;
 	public void onCreate(Bundle AddingNames) {
 		// TODO Auto generated method stub
 		super.onCreate(AddingNames);
 		setContentView(R.layout.activity_main);
 		et = (EditText) findViewById(R.id.et);
+		txt= (TextView) findViewById(R.id.txt);
 	}
 	
 	@Override
