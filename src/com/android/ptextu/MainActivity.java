e1b0e5d0b69cea14c619566e9d870b7265483526
Shubham Singh <shubhamsingh147@gmail.com>
2014-10-08 14:38:39 +0530

    Multi ET implemented

--- src/com/android/ptextu/MainActivity.java
+++ src/com/android/ptextu/MainActivity.java
@@ -16,46 +16,44 @@ import android.widget.Toast;
 
 import com.example.ptextu.R;
 
-public class MainActivity extends Activity implements OnClickListener {
+public class MainActivity extends Activity{
 	String finalString="";
-	EditText et;
+	EditText et,et2,et3;
 	TextView txt;
 	public void onCreate(Bundle AddingNames) {
 		// TODO Auto generated method stub
 		super.onCreate(AddingNames);
 		setContentView(R.layout.activity_main);
 		et = (EditText) findViewById(R.id.et);
+		et2 = (EditText) findViewById(R.id.et2);
+		et3 = (EditText) findViewById(R.id.et3);
 		txt= (TextView) findViewById(R.id.txt);
-		et.addTextChangedListener(watcher);
-		
+		et.addTextChangedListener(new GenericTextWatcher(et));
+		et2.addTextChangedListener(new GenericTextWatcher(et2));
+		et3.addTextChangedListener(new GenericTextWatcher(et3));
 	}
-	TextWatcher watcher= new TextWatcher() {
-		
-		@Override
-		public void onTextChanged(CharSequence s, int start, int before, int count) {
-			// TODO Auto-generated method stub
-			
-			txt.setText(s);
-		}
-		
-		@Override
-		public void beforeTextChanged(CharSequence s, int start, int count,
-				int after) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void afterTextChanged(Editable s) {
-			// TODO Auto-generated method stub
-			
-		}
-	};
-	@Override
-	public void onClick(DialogInterface dialog, int which) {
-		// TODO Auto-generated method stub
-		
-	}
-
 	
+	private class GenericTextWatcher implements TextWatcher{
+
+	    private View view;
+	    private GenericTextWatcher(View view) {
+	        this.view = view;
+	    }
+
+	    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+	    public void afterTextChanged(Editable editable) {}
+	    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+	    	  switch(view.getId()){
+	            case R.id.et:
+	                txt.setText(charSequence);
+	                break;
+	            case R.id.et2:
+	                txt.setText(charSequence);
+	                break;
+	            case R.id.et3:
+	                txt.setText(charSequence);
+	                break;
+	        }
+	    }
+	}
 }
